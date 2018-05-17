b4b44f9727b2d817f0e8729853d0d006ae03f19b
Claudio Soares <cslopes@gmail.com>
2018-05-17 14:54:52 -0300

    configuração dinâmica no servidor

--- src/main/java/com/playbackgg/ping/PingApplication.java
+++ src/main/java/com/playbackgg/ping/PingApplication.java
@@ -4,6 +4,7 @@ import com.playbackgg.ping.service.Producer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +14,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PingApplication {
 
     public static void main(String[] args) {

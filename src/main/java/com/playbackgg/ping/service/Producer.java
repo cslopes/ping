df4989f80fa779ce0f99733d1a3f2d11509a72d5
Claudio Soares <cslopes@gmail.com>
2018-05-17 16:29:05 -0300

    configurações separadas

--- src/main/java/com/playbackgg/ping/service/Producer.java
+++ src/main/java/com/playbackgg/ping/service/Producer.java
@@ -3,6 +3,8 @@ package com.playbackgg.ping.service;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.playbackgg.ping.data.DTO;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class);
     private final QueueMessagingTemplate queueMessagingTemplate;
 
-    @Value("sqs.queue.name")
+    @Value("${aws.queue.name}")
     String queueName;
+
 
     @Autowired
     public Producer(AmazonSQSAsync amazonSQS) {
@@ -21,6 +25,7 @@ public class Producer {
     }
 
     public void send(String message) {
+        log.info("{} - {}", queueName, message);
         queueMessagingTemplate.convertAndSend(queueName, new DTO(DateTime.now(), message));
     }
 
